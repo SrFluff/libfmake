3e01d6c0afa64a7956e4ee0b8e60d1a2d4abd4d3
Franco <fluffins007@outlook.com>
2025-12-11 21:36:52 -0500

    Added libfmake.py, and edited README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # libfmake
 fmake's config parser written as a library.
+
+# Functions
+
+`readConfig(configName)` - Returns the values of a config file in a tuple (cc,src,out,msg,say,run)\
+`genConfig(configName,overwrite,cc,src,out,msg,say,run)` - Generates a new fmake config with the specified values
---
+++ libfmake.py
@@ -0,0 +1,52 @@
+import os
+def readConfig(configName):
+    if not os.path.exists(configName):
+        print("aaa")
+        return -1
+    f = open(configName,"r")
+    cc = "gcc"
+    src = "main.c"
+    out = "main"
+    msg = True
+    say = "Compiling done!"
+    run = False
+    for i in f:
+        z = i.strip().split()
+        if len(z) == 0:
+            pass
+        if len(z) >= 3:
+            if z[0] == "cc":
+                cc = z[2]
+            if z[0] == "src":
+                src = z[2]
+            if z[0] == "out":
+                out = z[2]
+            if z[0] == "msg":
+                if z[2] == "true":
+                    msg = True
+                if z[2] == "false":
+                    msg = False
+            if z[0] == "say":
+                x = i.strip()
+                while x[0] != "=":
+                    x = x[1:]
+                x = x[1:]
+                while x[0] == " ":
+                    x = x[1:]
+                say = x
+            if z[0] == "run":
+                if z[2] == "true":
+                    run = True
+                if z[2] == "false":
+                    run = False
+    return (cc,src,out,msg,say,run)
+
+def genConfig(fileName="make.fmake",overWrite=False,gCC="gcc",gSRC="main.c",gOUT="main",gMSG=True,gSAY="Compiling done!",gRUN=False):
+    if os.path.exists(fileName) and not overWrite:
+        return -1
+    elif os.path.exists(fileName) and overWrite or not os.path.exists(fileName):
+        f = open(fileName,"w")
+        f.write(f"cc = {gCC}\nsrc = {gSRC}\nout = {gOUT}\nmsg = {str(gMSG).lower()}\nsay = {gSAY}\nrun = {str(gRUN).lower()}\n")
+        f.close()
+        return 0
+        
