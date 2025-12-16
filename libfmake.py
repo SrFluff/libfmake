622182d8bedf6dc51fceb707dccb336b40a4b167
franco <fluffins007@outlook.com>
2025-12-16 08:37:29 -0500

    Added mke and mkf to gen & readConfig

--- libfmake.py
+++ libfmake.py
@@ -10,6 +10,9 @@ def readConfig(configName):
     msg = True
     say = "Compiling done!"
     run = False
+    mke = False
+    mkf = "-j$(nproc)"
+
     for i in f:
         z = i.strip().split()
         if len(z) == 0:
@@ -39,14 +42,27 @@ def readConfig(configName):
                     run = True
                 if z[2] == "false":
                     run = False
-    return (cc,src,out,msg,say,run)
+            if z[0] == "mke":
+                if z[2] == "false":
+                    mke = False
+                if z[2] == "true":
+                    mke = True
+            if z[0] == "mkf":
+                x = i.strip()
+                while x[0] != "=":
+                    x = x[1:]
+                x = x[1:]
+                while x[0] == " ":
+                    x = x[1:]
+
+    return (cc,src,out,msg,say,run,mke,mkf)
 
 def genConfig(fileName="make.fmake",overWrite=False,gCC="gcc",gSRC="main.c",gOUT="main",gMSG=True,gSAY="Compiling done!",gRUN=False):
     if os.path.exists(fileName) and not overWrite:
         return -1
     elif os.path.exists(fileName) and overWrite or not os.path.exists(fileName):
         f = open(fileName,"w")
-        f.write(f"cc = {gCC}\nsrc = {gSRC}\nout = {gOUT}\nmsg = {str(gMSG).lower()}\nsay = {gSAY}\nrun = {str(gRUN).lower()}\n")
+        f.write(f"cc = {gCC}\nsrc = {gSRC}\nout = {gOUT}\nmsg = {str(gMSG).lower()}\nsay = {gSAY}\nrun = {str(gRUN).lower()}\nmke = {str(mke).lower()}\nmkf = {mkf}")
         f.close()
         return 0
         
